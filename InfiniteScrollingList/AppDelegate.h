e7c054baf8661565a3e815ffb6369db2c13c82f2
David de Tena <daviddetenacalvo@gmail.com>
2016-04-25 10:14:30 +0200

    Initial Commit

---
+++ InfiniteScrollingList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InfiniteScrollingList
+//
+//  Created by David de Tena on 25/04/16.
+//  Copyright © 2016 David de Tena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
